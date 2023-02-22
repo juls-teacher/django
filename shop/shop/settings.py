55ac52124de8e29848e351f8d0e76211f2f21313
juls-teacher <juliayurchyk@gmail.com>
2023-02-22 09:15:51 +0300

    add

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -72,12 +72,15 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   "default": {
+       "ENGINE": "django.db.backends.postgresql",
+       "NAME": "django",
+       "USER": "django",
+       "PASSWORD": "django",
+       "HOST": "localhost",
+       "PORT": 5432,
+   }
 }
 
 
