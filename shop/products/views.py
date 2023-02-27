78d3116bd06f77a602a310fcb8967804d7126773
juls-teacher <juliayurchyk@gmail.com>
2023-02-27 12:27:29 +0300

    view 3 params

--- shop/products/views.py
+++ shop/products/views.py
@@ -6,12 +6,12 @@ logger = logging.getLogger(__name__)
 
 
 def index(request):
-    if request.GET.get("param"):
-        logger.info(f' My custom var= {settings.MY_CUSTOM_VARIABLE}')
-        logger.info(f' My env var= {settings.MY_ENV_VARIABLE}')
-        logger.info(f' My param = {request.GET.get("param")}')
+    # if request.GET.get("param"):
+    #     logger.info(f' My custom var= {settings.MY_CUSTOM_VARIABLE}')
+    #     logger.info(f' My env var= {settings.MY_ENV_VARIABLE}')
+    #     logger.info(f' My param = {request.GET.get("param")}')
 
-    if int(os.getenv(key='FIRST_PARAM')) == 2023:
+    if int(os.getenv(key='FIRST_PARAM')) :
         logger.info(f'second variable  {os.getenv(key="SECOND_PARAM")}')
     else:
         logger.info(f'third variable {os.getenv(key="THIRD_PARAM")}')
