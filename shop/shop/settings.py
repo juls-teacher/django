8324880ba12ac19bc0bfdd0e5b975df87ec269e4
juls-teacher <juliayurchyk@gmail.com>
2023-02-27 12:13:47 +0300

    2 val add

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv(key='SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
-DEBUG = eval(os.getenv(key='DEBUG'))
+DEBUG = bool(os.getenv(key='DEBUG'))
 
 
 ALLOWED_HOSTS = []
