c4723eb2aac3175808fcb3eaf2f6a8e7c56499ca
juls-teacher <juliayurchyk@gmail.com>
2023-02-28 23:19:00 +0300

    add class product and update admin

--- shop/products/models.py
+++ shop/products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(
+        auto_now_add=True, db_index=True
+    )
--- shop/profiles/admin.py
+++ shop/profiles/admin.py
@@ -4,10 +4,19 @@ from django.contrib import admin
 from django.contrib import admin
 
 from profiles.models import Profile
+from products.models import Product
 
 @admin.register(Profile)
-class ProductAdmin(admin.ModelAdmin):
+class ProfileAdmin(admin.ModelAdmin):
    list_display = ("user", "first_name", "last_name", "created_at")
    fields = ("user", "first_name", "last_name", "age","created_at")
    readonly_fields = ("created_at",)
    search_fields = ("first_name", "last_name")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+   list_display = ("title", "price", "description", "created_at")
+   fields = ("title", "price", "description", "created_at")
+   readonly_fields = ("created_at",)
+   search_fields = ("title", "price")
