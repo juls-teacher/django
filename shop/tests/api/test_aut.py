1e4597d2d2b591260d75e1342f5c87e0cfd056e5
juls-teacher <juliayurchyk@gmail.com>
2023-06-06 00:53:28 +0300

    update test

--- shop/tests/api/test_aut.py
+++ shop/tests/api/test_aut.py
@@ -1,10 +1,8 @@
 import pytest
-# from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.admin import User
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 
-# from products.models import Product
 
 @pytest.mark.django_db
 class TestProductsApi:
@@ -28,6 +26,6 @@ class TestProductsApi:
         self.client.credentials(
             HTTP_AUTHORIZATION=f"Token {response.json().get('token')}"
         )
-        # response = self.client.delete("/api/logout/")
-        # assert response.status_code == 200
-        # assert not Token.objects.exists()
+        response = self.client.delete("/api/logout/")
+        assert response.status_code == 200
+        assert not Token.objects.exists()
